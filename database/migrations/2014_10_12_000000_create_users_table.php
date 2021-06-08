9c27f94dd830344744368d624cd98bf13cabb1c6
Hoya <angelring127@gmail.com>
2021-06-09 00:37:34 +0900

    Update: telescope, frontend scafollding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedInteger('role')->comment('역활');
             $table->rememberToken();
             $table->timestamps();
         });
